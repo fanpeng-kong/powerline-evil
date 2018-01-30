9b528fe0f911858a0ea242fb4e309959192a34c8
Fanpeng Kong <kshark343@gmail.com>
2018-01-30 16:10:49 +1100

    Add vim powerline-like theme

--- powerline-evil-themes.el
+++ powerline-evil-themes.el
@@ -179,5 +179,288 @@
                              (powerline-fill mode-line (powerline-width rhs))
                              (powerline-render rhs)))))))
 
+;; Powerline themes
+(defcustom powerline-display-buffer-size t
+  "When non-nil, display the buffer size."
+  :group 'powerline
+  :type 'boolean)
+
+(defcustom powerline-display-mule-info t
+  "When non-nil, display the mule info."
+  :group 'powerline
+  :type 'boolean)
+
+(defcustom powerline-display-hud t
+  "When non-nil, display the hud."
+  :group 'powerline
+  :type 'boolean)
+
+(defcustom powerline-utf-8-subseparator-left #x2b81
+  "The unicode character number for the left facing subseparator."
+  :group 'powerline
+  :type  '(choice integer (const nil)))
+
+(defcustom powerline-utf-8-subseparator-right #x2b83
+  "The unicode character number for the right facing subseparator."
+  :group 'powerline
+  :type  '(choice integer (const nil)))
+
+ (defface powerline-file-name-active
+   '((t (:foreground "#ffffff" :background "#585858" :weight bold :inherit mode-line)))
+   "Powerline file name face"
+   :group 'powerline)
+
+ (defface powerline-file-name-inactive
+   '((t (:foreground "#808080" :background "#262626" :weight bold :inherit mode-line)))
+   "Powerline file name face"
+   :group 'powerline)
+
+ (defface powerline-major-mode-active
+   '((t (:foreground "#ffffff" :background "#303030" :inherit mode-line)))
+   "Powerline major mode face"
+   :group 'powerline)
+
+ (defface powerline-major-mode-inactive
+   '((t (:foreground "#808080" :background "#121212" :inherit mode-line)))
+   "Powerline major mode face"
+   :group 'powerline)
+
+ (defface powerline-background-active
+   '((t (:foreground "#bcbcbc" :background "#303030" :inherit mode-line)))
+   "Powerline background face"
+   :group 'powerline)
+
+ (defface powerline-background-inactive
+   '((t (:foreground "#585858" :background "#121212" :inherit mode-line)))
+   "Powerline background face"
+   :group 'powerline)
+
+ (defface powerline-line-percent-active
+   '((t (:foreground "#51b000" :background "#585858" :inherit mode-line)))
+   "Powerline line percent face"
+   :group 'powerline)
+
+ (defface powerline-line-percent-inactive
+   '((t (:foreground "#626262" :background "#262626" :inherit mode-line)))
+   "Powerline line percent face"
+   :group 'powerline)
+
+ (defface powerline-position-active
+   '((t (:foreground "#262626" :background "#d0d0d0" :inherit mode-line)))
+   "Powerline position face"
+   :group 'powerline)
+
+ (defface powerline-position-inactive
+   '((t (:foreground "#262626" :background "#626262" :inherit mode-line)))
+   "Powerline position face"
+   :group 'powerline)
+
+ (defface powerline-line-current-active
+   '((t (:foreground "#262626" :background "#d0d0d0" :weight bold :inherit mode-line)))
+   "Powerline current line face"
+   :group 'powerline)
+
+ (defface powerline-line-current-inactive
+   '((t (:foreground "#262626" :background "#626262" :weight bold :inherit mode-line)))
+   "Powerline current line face"
+   :group 'powerline)
+
+ (defface powerline-col-current-active
+   '((t (:foreground "#006000" :background "#d0d0d0" :inherit mode-line)))
+   "Powerline current column face"
+   :group 'powerline)
+
+ (defface powerline-col-current-inactive
+   '((t (:foreground "#006000" :background "#626262" :inherit mode-line)))
+   "Powerline current column face"
+   :group 'powerline)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; these next faces are for the status indicator
+;; read-only buffer
+(defface powerline-read-only-active
+  '((t (:foreground "#cebff3" :background "#585858" :inherit mode-line)))
+  "Powerline buffer read only face"
+  :group 'powerline)
+
+(defface powerline-read-only-inactive
+  '((t (:foreground "#cebff3" :background "#262626" :inherit mode-line)))
+  "Powerline buffer read only face"
+  :group 'powerline)
+
+;; modified buffer
+(defface powerline-modified-active
+  '((t (:foreground "#FF2600" :background "#585858" :inherit mode-line)))
+  "Powerline buffer modified face"
+  :group 'powerline)
+
+(defface powerline-modified-inactive
+  '((t (:foreground "#FF2600" :background "#262626" :inherit mode-line)))
+  "Powerline buffer modified face"
+  :group 'powerline)
+
+;; unmodified buffer
+(defface powerline-unmodified-active
+  '((t (:foreground  "#cebff3" :background  "#585858" :inherit mode-line)))
+  "Powerline buffer unmodified face"
+  :group 'powerline)
+
+(defface powerline-unmodified-inactive
+  '((t (:foreground "#cebff3" :background "#262626" :inherit mode-line)))
+  "Powerline buffer unmodified face"
+  :group 'powerline)
+
+;; the remote indicator
+;; (defface powerline-remote-active
+;;   '((t (:foreground (face-attribute 'font-lock-comment-face :foreground) :background (face-attribute 'default :background) :inherit mode-line)))
+;;   "Powerline buffer remote face"
+;;   :group 'powerline)
+;; 
+;; (defface powerline-remote-inactive
+;;   '((t (:foreground (face-attribute 'font-lock-comment-face :foreground) :background (face-attribute 'default :background) :inherit mode-line)))
+;;   "Powerline buffer remote face"
+;;   :group 'powerline)
+
+(defface powerline-remote-active
+  '((t (:foreground "#cebff3" :background "#585858" :inherit mode-line)))
+  "Powerline buffer remote face"
+  :group 'powerline)
+
+(defface powerline-remote-inactive
+  '((t (:foreground "#cebff3" :background "#585858" :inherit mode-line)))
+  "Powerline buffer remote face"
+  :group 'powerline)
+
+(defun shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
+
+(defpowerline kong-powerline-narrow
+  (let (real-point-min real-point-max)
+    (save-excursion
+      (save-restriction
+        (widen)
+        (setq real-point-min (point-min) real-point-max (point-max))))
+    (when (or (/= real-point-min (point-min))
+              (/= real-point-max (point-max)))
+      (propertize (concat (char-to-string #x2691) " Narrow")
+                  'mouse-face 'mode-line-highlight
+                  'help-echo "mouse-1: Remove narrowing from the current buffer"
+                  'local-map (make-mode-line-mouse-map
+                              'mouse-1 'mode-line-widen)))))
+
+(defpowerline kong-powerline-vc
+  (when (and (buffer-file-name (current-buffer)) vc-mode)
+    (if window-system
+        (let ((backend (vc-backend (buffer-file-name (current-buffer)))))
+          (when backend
+            (format "%s %s: %s"
+                    (char-to-string #xe0a0)
+                    backend
+                    (vc-working-revision (buffer-file-name (current-buffer)) backend)))))))
+
+;;;###autoload
+(defun powerline-evil-vim-powerline-theme ()
+  "Setup a mode-line with major, evil mode on the left."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+
+			  ;; toggle faces between active and inactive
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+
+			  (face-file-name (if active 'powerline-file-name-active 'powerline-file-name-inactive))
+			  (face-major-mode (if active 'powerline-major-mode-active 'powerline-major-mode-inactive))
+			  (face-background (if active 'powerline-background-active 'powerline-background-inactive))
+			  (face-line-percent (if active 'powerline-line-percent-active 'powerline-line-percent-inactive))
+			  (face-position (if active 'powerline-position-active 'powerline-position-inactive))
+			  (face-line-current (if active 'powerline-line-current-active 'powerline-line-current-inactive))
+			  (face-col-current (if active 'powerline-col-current-active 'powerline-col-current-inactive))
+
+			  (face-read-only (if active 'powerline-read-only-active 'powerline-read-only-inactive))
+			  (face-modified (if active 'powerline-modified-active 'powerline-modified-inactive))
+			  (face-unmodified (if active 'powerline-unmodified-active 'powerline-unmodified-inactive))
+			  (face-remote (if active 'powerline-remote-active 'powerline-remote-inactive))
+
+			  ;; get the separators
+                          (separator-left (intern (format "powerline-%s-%s"
+							  (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+			  ;; the left side
+                          (lhs
+			   (append
+			    ;; Evil states
+			    (let ((evil-face (powerline-evil-face active)))
+			      (list (powerline-raw (powerline-evil-tag) evil-face)
+				    (funcall separator-left evil-face face-file-name)))
+
+			    (list (cond (buffer-read-only
+			     		 (powerline-raw (concat (char-to-string #xe0a2) " ") face-read-only 'l))
+			     		((buffer-modified-p)
+			     		 (if (not (bound-and-true-p ml-interactive?))
+			     		     (powerline-raw (concat (char-to-string #x2621) " ") face-modified 'l)
+			     		   (powerline-raw (concat (char-to-string #x259e) " ") face-unmodified 'l)))
+			     		((not (buffer-modified-p))
+			     		 (powerline-raw (concat (char-to-string #x26c1) " ") face-unmodified 'l)))
+				  ;; remote indicator
+				  (when (file-remote-p default-directory)
+				    (powerline-raw (concat " " (char-to-string #x211b)) face-remote))
+				  
+				  (powerline-buffer-id face-file-name 'r)
+				  ;; (when (and (boundp 'which-func-mode) which-func-mode)
+				  ;;   (powerline-raw which-func-format face-file-name 'l))
+
+				  (funcall separator-left face-file-name face-major-mode)
+
+				  ;; (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+				  ;;   (powerline-raw erc-modified-channels-object face1 'l))
+				  (powerline-major-mode face-major-mode 'l)
+				  (powerline-process face-major-mode)
+				  (powerline-raw (char-to-string powerline-utf-8-subseparator-left) face-background 'l)
+
+				  (powerline-vc face-background 'r)
+				  (when (bound-and-true-p nyan-mode)
+				    (powerline-raw (list (nyan-create)) face-background 'l)))))
+
+                          (rhs (list (powerline-raw mode-line-misc-info face-background 'r)
+
+				(powerline-raw (char-to-string powerline-utf-8-subseparator-right) face-background 'l)
+				(when powerline-display-buffer-size
+				  (powerline-buffer-size face-background 'l))
+				(when powerline-display-mule-info
+				  (powerline-raw mode-line-mule-info face-background 'l))
+				(powerline-raw " " face-background)
+
+				(funcall separator-right face-background face-line-percent)
+				(powerline-raw " " face-line-percent)
+				(powerline-raw "%p" face-line-percent 'r)
+				(funcall separator-right face-line-percent face-position)
+				(unless window-system
+				  (powerline-raw (char-to-string #xe0a1) face-position 'l))
+				(powerline-raw "%2l" face-line-current 'l)
+				(powerline-raw ":" face-position 'l)
+				(powerline-raw "%2c" face-col-current 'r)
+				(when powerline-display-hud
+				  (powerline-hud face-background face-major-mode)))))
+
+		     (concat (powerline-render lhs)
+			     (powerline-fill face-background (powerline-width rhs))
+			     (powerline-render rhs)))))))
+
 (provide 'powerline-evil-themes)
 ;;; powerline-evil-themes.el ends here
--- powerline-evil.el
+++ powerline-evil.el
@@ -19,77 +19,143 @@
 (require 'powerline)
 (require 'powerline-evil-themes)
 
-(defcustom powerline-evil-tag-style 'visual-expanded
-  "The style to use for displaying the evil state tag.
+;; (defcustom powerline-evil-tag-style 'visual-expanded
+;;   "The style to use for displaying the evil state tag.
 
-Valid Values: standard, verbose, visual-expanded"
-  :group 'powerline
-  :type '(choice (const standard)
-                 (const verbose)
-                 (const visual-expanded)))
+;; Valid Values: standard, verbose, visual-expanded"
+;;   :group 'powerline
+;;   :type '(choice (const standard)
+;;                  (const verbose)
+;;                  (const visual-expanded)))
 
-(defface powerline-evil-base-face
-  '((t (:foreground "white" :inherit mode-line)))
-  "Base face for powerline evil faces."
-  :group 'powerline)
+;; (defface powerline-evil-base-face
+;;   '((t (:foreground "white" :inherit mode-line)))
+;;   "Base face for powerline evil faces."
+;;   :group 'powerline)
 
-(defface powerline-evil-normal-face
-  '((t (:background "green" :inherit powerline-evil-base-face)))
-  "Powerline face for evil NORMAL state."
-  :group 'powerline)
+;; (defface powerline-evil-normal-face
+;;   '((t (:background "green" :inherit powerline-evil-base-face)))
+;;   "Powerline face for evil NORMAL state."
+;;   :group 'powerline)
 
-(defface powerline-evil-insert-face
-  '((t (:background "blue" :inherit powerline-evil-base-face)))
-  "Powerline face for evil INSERT state."
-  :group 'powerline)
+;; (defface powerline-evil-insert-face
+;;   '((t (:background "blue" :inherit powerline-evil-base-face)))
+;;   "Powerline face for evil INSERT state."
+;;   :group 'powerline)
 
-(defface powerline-evil-visual-face
-  '((t (:background "orange" :inherit powerline-evil-base-face)))
-  "Powerline face for evil VISUAL state."
-  :group 'powerline)
-
-(defface powerline-evil-operator-face
-  '((t (:background "cyan" :inherit powerline-evil-operator-face)))
-  "Powerline face for evil OPERATOR state."
-  :group 'powerline)
-
-(defface powerline-evil-replace-face
-  '((t (:background "red" :inherit powerline-evil-base-face)))
-  "Powerline face for evil REPLACE state."
-  :group 'powerline)
-
-(defface powerline-evil-motion-face
-  '((t (:background "magenta" :inherit powerline-evil-base-face)))
-  "Powerline face for evil MOTION state."
-  :group 'powerline)
-
-(defface powerline-evil-emacs-face
-  '((t (:background "violet" :inherit powerline-evil-base-face)))
-  "Powerline face for evil EMACS state."
-  :group 'powerline)
+;; (defface powerline-evil-visual-face
+;;   '((t (:background "orange" :inherit powerline-evil-base-face)))
+;;   "Powerline face for evil VISUAL state."
+;;   :group 'powerline)
 
 
-;;;###autoload
-(defun powerline-evil-face ()
-  "Function to select appropriate face based on `evil-state'."
-  (let* ((face (intern (concat "powerline-evil-" (symbol-name evil-state) "-face"))))
-    (if (facep face) face nil)))
+;; (defface powerline-evil-operator-face
+;;   '((t (:background "cyan" :inherit powerline-evil-operator-face)))
+;;   "Powerline face for evil OPERATOR state."
+;;   :group 'powerline)
 
-(defun powerline-evil-tag ()
-  "Get customized tag value for current evil state."
-  (let* ((visual-block (and (evil-visual-state-p)
-                            (eq evil-visual-selection 'block)))
-         (visual-line (and (evil-visual-state-p)
-                           (eq evil-visual-selection 'line))))
-    (cond ((eq powerline-evil-tag-style 'visual-expanded)
-           (cond (visual-block " +V+ ")
-                 (visual-line " -V- ")
-                 (t evil-mode-line-tag)))
-          ((eq powerline-evil-tag-style 'verbose)
-           (upcase (concat (symbol-name evil-state)
-                           (cond (visual-block " BLOCK")
-                                 (visual-line " LINE")))))
-          (t evil-mode-line-tag))))
+;; (defface powerline-evil-replace-face
+;;   '((t (:background "red" :inherit powerline-evil-base-face)))
+;;   "Powerline face for evil REPLACE state."
+;;   :group 'powerline)
+
+;; (defface powerline-evil-motion-face
+;;   '((t (:background "magenta" :inherit powerline-evil-base-face)))
+;;   "Powerline face for evil MOTION state."
+;;   :group 'powerline)
+
+;; (defface powerline-evil-emacs-face
+;;   '((t (:background "violet" :inherit powerline-evil-base-face)))
+;;   "Powerline face for evil EMACS state."
+;;   :group 'powerline)
+
+
+;; ;;;###autoload
+;; (defun powerline-evil-face ()
+;;   "Function to select appropriate face based on `evil-state'."
+;;   (let* ((face (intern (concat "powerline-evil-" (symbol-name evil-state) "-face"))))
+;;     (if (facep face) face nil)))
+
+;; (defun powerline-evil-tag ()
+;;   "Get customized tag value for current evil state."
+;;   (let* ((visual-block (and (evil-visual-state-p)
+;;                             (eq evil-visual-selection 'block)))
+;;          (visual-line (and (evil-visual-state-p)
+;;                            (eq evil-visual-selection 'line))))
+;;     (cond ((eq powerline-evil-tag-style 'visual-expanded)
+;;            (cond (visual-block " +V+ ")
+;;                  (visual-line " -V- ")
+;;                  (t evil-mode-line-tag)))
+;;           ((eq powerline-evil-tag-style 'verbose)
+;;            (upcase (concat (symbol-name evil-state)
+;;                            (cond (visual-block " BLOCK")
+;;                                  (visual-line " LINE")))))
+;;           (t evil-mode-line-tag))))
+
+;; Evil mode
+(eval-after-load 'evil
+  '(progn
+     (defface powerline-evil-normal-face
+       '((t (:foreground "#005f00" :background "#afd700" :weight bold :inherit mode-line)))
+       "face to fontify evil normal state"
+       :group 'powerline)
+
+     (defface powerline-evil-insert-face
+       '((t (:foreground "#005f5f" :background "#ffffff" :weight bold :inherit mode-line)))
+       "face to fontify evil insert state"
+       :group 'powerline)
+
+     (defface powerline-evil-visual-face
+       '((t (:foreground "#080808" :background "#df5f00" :weight bold :inherit mode-line)))
+       "face to fontify evil visual state"
+       :group 'powerline)
+
+     (defface powerline-evil-replace-face
+       '((t (:foreground "#ffffff" :background "#d70000" :weight bold :inherit mode-line)))
+       "face to fontify evil replace state"
+       :group 'powerline)
+
+     (defface powerline-evil-emacs-face
+       '((t (:foreground "#d7d7ff" :background "#5f00af" :weight bold :inherit mode-line)))
+       "face to fontify evil emacs state"
+       :group 'powerline)
+
+     (defface powerline-evil-motion-face
+       '((t (:foreground "white" :background "blue" :weight bold :inherit mode-line)))
+       "face to fontify evil motion state"
+       :group 'powerline)
+
+     (defface powerline-evil-operator-face
+       '((t (:foreground "white" :background "sky blue" :weight bold :inherit mode-line)))
+       "face to fontify evil operator state"
+       :group 'powerline)
+
+     (defface powerline-evil-iedit-face
+       '((t (:foreground "white" :background "firebrick" :weight bold :inherit mode-line)))
+       "face to fontify evil iedit state"
+       :group 'powerline)
+
+     (defface powerline-evil-iedit-insert-face
+       '((t (:foreground "white" :background "#0F9" :weight bold :inherit mode-line)))
+       "face to fontify evil iedit insert state"
+       :group 'powerline)
+
+     (defun powerline-evil-face (active)
+       (let ((face (intern (concat "powerline-evil-" (symbol-name evil-state) "-face"))))
+         (cond ((and active (facep face))
+                face)
+               (active 'powerline-file-name-active)
+               (t 'powerline-file-name-inactive))))
+
+     (defun powerline-evil-tag ()
+       (cond
+        ((and (evil-visual-state-p) (eq evil-visual-selection 'block))
+         " +V+ ")
+        ((and (evil-visual-state-p) (eq evil-visual-selection 'line))
+         " -V- ")
+        (t
+         evil-mode-line-tag)))
+     ))
 
 (provide 'powerline-evil)
 ;;; powerline-evil.el ends here
